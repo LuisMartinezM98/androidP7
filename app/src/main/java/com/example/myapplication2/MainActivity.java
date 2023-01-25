17121ada5dca5d7e1a9870c739ace5d229634a33
LuisMartinezM98 <luiizmartinez@gmail.com>
2023-01-25 08:29:53 -0600

    event Hello world with litener event on Button element done

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    void sayHello(View v){
+        System.out.println("Hello");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button objButton = (Button) findViewById(R.id.button);
+        assert objButton != null;
+        objButton.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view) {
+               System.out.println("Hello world");
+           }
+        });
     }
+
+
 }
